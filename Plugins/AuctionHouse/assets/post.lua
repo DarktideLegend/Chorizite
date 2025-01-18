6905c3f02610fe3e771a590f4d9ae0998ffe1395
pourman <apourman@protonmail.com>
2025-01-17 20:07:22 -0800

    feat: validate workmanship for currency type

--- Plugins/AuctionHouse/assets/post.lua
+++ Plugins/AuctionHouse/assets/post.lua
@@ -357,6 +357,14 @@ local PostFormTitle = function(state)
   })
 end
 
+local PostFormError = function(state) 
+  return rx:Div({ class = "post-form-item-container" }, {
+    rx:Div({ class = "post-form-item" }, {
+      rx:Div({ class = "post-form-error" }, state.dragError),
+    })
+  })
+end
+
 local PostForm = function(state)
   return rx:Div({ class = "post-form" }, {
     rx:Div({ class = {
